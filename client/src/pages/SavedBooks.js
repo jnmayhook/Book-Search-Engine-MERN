6c57c9d63b101020e9abc7a18c1a63122b33e131
Nathan Mayhook <jnmayhook@gmail.com>
2022-09-01 22:07:07 -0700

    deploying

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
 
-import { SAVE_BOOK } from '../utils/mutations'
-
+import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect('mongodb+srv://jnmayhook:xYYG1HW2qTzndVsG@cluster0.6drdrzs.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
